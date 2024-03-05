2c0dbab9c187f78a64d0904850675f2b23ac9264
ari <arielsebastian2006@gmail.com>
2024-03-05 12:06:27 -0600

    Update sketch.js

--- A1 character/sketch.js
+++ A1 character/sketch.js
@@ -12,4 +12,20 @@ function setup() {
 
 function draw() {
   background(220);
+  fill(251,222,192);
+  circle(190,220,150);
+  rect(146,295,90);
+  rect(126,325,20);
+  rect(236,325,20);
+  fill(241,215,179);
+  circle(160,380,48);
+  circle(220,380,48);
+  fill(208,234,234);
+  circle(152,225,45);
+  circle(225,225,45);
+  fill(251,222,192);
+  circle(100,310,50);
+
+
+
 }
