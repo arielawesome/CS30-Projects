6ab2057ca78ae448be6b9c7680e16bf4e4c31bfd
ari <arielsebastian2006@gmail.com>
2024-03-12 12:11:34 -0600

    bouncing ball

--- ObjectDemo/sketch.js
+++ ObjectDemo/sketch.js
@@ -6,18 +6,13 @@
 // - describe what you did to take this project "above and beyond"
 
 
-let newBall;
 
+
+let ballsArray = []
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  createBall();
  
-  newBall = {
-    x: random(width),
-    y: random(height),
-    diameter: random(25, 100),
-    dx: random(-5, 5),
-    dy: random(-5, 5),
-  };
  
 }
 
@@ -25,9 +20,27 @@ function draw() {
   background(220);
 
   fill(255,0,0);
-  circle(newBall.x, newBall.y, newBall.diameter);
 
-newBall.x += newBall.dx;
-newBall.y += newBall.dx;
+  for (let i = 0; i < ballsArray.length; i++) {
+  circle(ballsArray[i].x, ballsArray[i].y, ballsArray[i].diameter);
+ballsArray[i].x += ballsArray[i].dx;
+ballsArray[i].y += ballsArray[i].dy;
+  }
+}
 
+function createBall() {
+  newBall = {
+    x: random(width),
+    y: random(height),
+    diameter: random(25, 100),
+    dx: random(-2, 2),
+    dy: random(-2, 2),
+  };
+ballsArray.push(newBall);
+}
+function mousePressed() {
+  createBall();
+  ballsArray[ballsArray.length-1].x = mouseX;
+  ballsArray[ballsArray.length-1].y - mouseY;
+  
 }
